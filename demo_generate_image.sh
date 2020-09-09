e4779f47fc168d251c2d8e71d35fe3b21d0492d3
leeyegy <35657511+leeyegy@users.noreply.github.com>
2020-09-09 14:33:27 +0800

    Update demo_generate_image.sh

--- demo_generate_image.sh
+++ demo_generate_image.sh
@@ -1,7 +1,7 @@
-for attack in FGSM PGD Momentum CW DeepFool
+for attack in FGSM 
 do 
 	for epsilon in 0.03137 0.06275 0.00784
 	do 
-	python data_generator.py --attack_method $attack --epsilon $epsilon 
+	python data_generator.py --task g_img --attack_method $attack --epsilon $epsilon 
 done 
 done 
