6951303a7a86ba1bf3e92309f778209c57e64115
leeyegy <35657511+leeyegy@users.noreply.github.com>
2020-09-09 14:52:32 +0800

    Update demo_adaptive_cifar10.sh

--- demo_adaptive_cifar10.sh
+++ demo_adaptive_cifar10.sh
@@ -1,10 +1,10 @@
-for attack_method in NONE
+for attack_method in FGSM
 do
-for epsilon in  0.0
+for epsilon in  0.00784 0.03137 0.06275
 do 
 for iters in  3
 do 
-	python main_cifar10_adaptive.py  --epsilon $epsilon --test_samples 500  --attack_method $attack_method --nb_iters $iters | tee ./logs/adaptive/v16/cifar10_new_$attack_method\_$epsilon.txt
+	python main_cifar10_adaptive.py  --epsilon $epsilon --test_samples 500  --attack_method $attack_method --nb_iters $iters | tee ./logs/adaptive/cifar10_$attack_method\_$epsilon.txt
 done
 done
 done
